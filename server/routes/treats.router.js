4dcd41a5013bc4baf640f5303cc71f879080dd87
Ailea Patrinely <aileapatrinely@gmail.com>
2020-07-13 13:27:07 -0500

    adds GET to router

--- server/routes/treats.router.js
+++ server/routes/treats.router.js
@@ -1,9 +1,20 @@
 const router = require('express').Router();
 const pool = require('../modules/pool');
 
-
 // GET /treats
-
+router.get('/', (req, res) => {
+  const queryText = `SELECT * FROM "treats";`;
+  pool
+    .query(queryText)
+    .then((dbResponse) => {
+      console.log(dbResponse);
+      res.send(dbResponse.rows);
+    })
+    .catch((err) => {
+      console.log('GET errror:', err);
+      res.sendStatus(500);
+    });
+});
 // POST /treats
 
 // PUT /treats/<id>
